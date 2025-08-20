50fb7946a3517f167398e5be6736f12d88d365f1
nsisthan@gmail.com <162958339+evenatthaphan@users.noreply.github.com>
2025-08-20 14:00:23 +0700

    update

--- server.ts
+++ server.ts
@@ -1,10 +1,11 @@
 import http from "http";
 import { app } from "./app";
 
-const port = process.env.port || 3306;
+const port = process.env.PORT || 3000;
+
 const server = http.createServer(app);
 
 //start server at port number
-server.listen(port,()=>{
-    console.log("Server is Started");
+server.listen(port, () => {
+    console.log(`Server is Started on port ${port}`);
 });

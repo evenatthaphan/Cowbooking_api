fb6008c0255ae22946c7fa8546cdebf817de2904
nsisthan@gmail.com <162958339+evenatthaphan@users.noreply.github.com>
2025-09-09 12:32:09 +0700

    update

--- app.ts
+++ app.ts
@@ -5,18 +5,18 @@ import { router as index } from "./api/index";
 import { router as farmers } from "./api/farmers";
 import { router as vetexpert } from "./api/VetExpert";
 import { router as usedtogether } from "./api/usedTogether";
-//import cors from "cors";
+import cors from "cors";
 
 // Object app => webApi
 export const app = express();
 
-// app.use(
-//     cors({
-//       origin: "*",
-//     })
-//   );
-// app.use(bodyParser.text()); 
-// app.use(bodyParser.json()); 
+app.use(
+    cors({
+      origin: "*",
+    })
+  );
+app.use(bodyParser.text()); 
+app.use(bodyParser.json()); 
 
 // app.use("/", (req, res) => {
 //   res.send("Hello World!!!");

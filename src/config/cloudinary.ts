934df86dc517ca6c31d0ea41b8dedc8a8c1ddb5b
nsisthan@gmail.com <162958339+evenatthaphan@users.noreply.github.com>
2025-09-26 13:09:00 +0700

    update

---
+++ src/config/cloudinary.ts
@@ -0,0 +1,9 @@
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: "dcqlzm5fp",
+  api_key: "117921789115259",
+  api_secret: "QpK6naJwKs7FGFIxjusn_Txdm8k",
+});
+
+export default cloudinary;

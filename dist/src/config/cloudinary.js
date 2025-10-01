46e7c282d8f7cac9cbb71cfee26a71d7ecb23307
nsisthan@gmail.com <162958339+evenatthaphan@users.noreply.github.com>
2025-10-01 14:13:20 +0700

    Add .gitignore and remove sensitive files

--- dist/src/config/cloudinary.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const cloudinary_1 = require("cloudinary");
-cloudinary_1.v2.config({
-    cloud_name: "dcqlzm5fp",
-    api_key: "117921789115259",
-    api_secret: "QpK6naJwKs7FGFIxjusn_Txdm8k",
-});
-exports.default = cloudinary_1.v2;

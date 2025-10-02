cba1f3cb7805b60bc5530fb7672e832ee302b300
nsisthan@gmail.com <162958339+evenatthaphan@users.noreply.github.com>
2025-10-02 15:03:49 +0700

    update

--- firebaseconnect.ts
+++ firebaseconnect.ts
@@ -1,3 +1,22 @@
+// import admin from "firebase-admin";
+
+// var admin = require("firebase-admin");
+
+// var serviceAccount = require("path/to/serviceAccountKey.json");
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: "https://flutter-57982-default-rtdb.asia-southeast1.firebasedatabase.app"
+// });
+
+
+// console.log(admin.app().options.projectId);
+
+// console.log("Project ID in service account:", serviceAccount.project_id);
+// export const db = admin.firestore();
+// export const serverTimestamp = admin.firestore.FieldValue.serverTimestamp;
+
+
 import admin from "firebase-admin";
 
 if (!process.env.FIREBASE_ADMIN_SDK) {
@@ -5,9 +24,7 @@ if (!process.env.FIREBASE_ADMIN_SDK) {
 }
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_SDK);
-
-// แปลง \n เป็น newline จริง
-serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
 
 if (!admin.apps.length) {
   admin.initializeApp({
@@ -15,8 +32,8 @@ if (!admin.apps.length) {
   });
 }
 
-console.log(admin.app().options.projectId);
+console.log("Admin SDK projectId:", admin.app().options.projectId);
+console.log("Service account project_id:", serviceAccount.project_id);
 
-console.log("Project ID in service account:", serviceAccount.project_id);
 export const db = admin.firestore();
 export const serverTimestamp = admin.firestore.FieldValue.serverTimestamp;
